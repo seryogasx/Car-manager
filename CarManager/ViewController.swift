941dc7cf219c510b08c06fc1d281fee40e50ee20
seryogasx <sergio.petrovx@yandex.ru>
2021-10-23 22:43:06 +0300

    main screen demo

--- CarManager/ViewController.swift
+++ CarManager/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var VehicleListTableView: UITableView!
+    
+    let vehicleTableViewCellIdentifier = "VehicleTableViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        VehicleListTableView.delegate = self
+        VehicleListTableView.dataSource = self
+        VehicleListTableView.register(UINib(nibName: "VehicleTableViewCell", bundle: nil), forCellReuseIdentifier: vehicleTableViewCellIdentifier)
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: vehicleTableViewCellIdentifier, for: indexPath) as? VehicleTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.setup(vehicleName: "SomeName")
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
